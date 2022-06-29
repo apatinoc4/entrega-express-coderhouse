3a22c8a6ab9d0258e1853f7f682f0e208645b4b8
Andres Patino <apatinoc4@gmail.com>
2022-06-28 20:06:45 -0500

    entrega 3 coderhouse

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const classContenedor = require("./classContenedor");
+
+const file1 = new classContenedor("productos.txt");
+
+const app = express();
+
+const PORT = 8080;
+
+const server = app.listen(PORT, () => {
+  console.log(`Servidor http escuchando en el puerto ${server.address().port}`);
+});
+server.on("error", (error) => console.log(`Error en servidor ${error}`));
+
+app.get("/productos", async (req, res) => {
+  const arrayProductos = await file1.getAll();
+
+  res.send(arrayProductos);
+});
+
+app.get("/productoRandom", async (req, res) => {
+  const arrayProductos = await file1.getAll();
+
+  res.send(arrayProductos[Math.floor(Math.random() * arrayProductos.length)]);
+});
